cd50c61c2f0ab3b3b5fe8209136df7e5401c475c
Jônatas Souza Soares <56240097+jonatasJS@users.noreply.github.com>
2021-09-17 14:16:44 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,14 +1,19 @@
-module.exports = async (client) => {
-  client.Ready = true, 
-  client.user.setPresence({
-    status: "online",  // You can show online, idle, and dnd
-    activity: {
-        name: "Music ( >play )",  // The message shown
-        type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING,
-    }
-});
+let name=true;
+
+setInterval(() => {
+  module.exports = async (client) => {
+    client.Ready = true, 
+    client.user.setPresence({
+      status: "online",  // You can show online, idle, and dnd
+      activity: {
+          name: name ? "Music (>play)" : "Help (>help)",  // The message shown
+          type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING,
+        }
+    });
     client.Manager.init(client.user.id);
     client.log("Successfully Logged in as " + client.user.tag); // You can change the text if you want, but DO NOT REMOVE "client.user.tag"
-client.RegisterSlashCommands();
-};
-
+    client.RegisterSlashCommands();
+  };
+  
+  name = !name;
+}, 10000);
