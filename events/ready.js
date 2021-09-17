5463b799e424200237dca837d0574dce346d2100
Jônatas Souza Soares <56240097+jonatasJS@users.noreply.github.com>
2021-09-17 14:20:35 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,11 @@
 let name=true;
 
 setInterval(() => {
-  module.exports = async (client) => {
-    client.Ready = true, 
+  name = !name;
+}, 10000);
+
+module.exports = async (client) => {
+    client.Ready = true;
     client.user.setPresence({
       status: "online",  // You can show online, idle, and dnd
       activity: {
@@ -13,7 +16,4 @@ setInterval(() => {
     client.Manager.init(client.user.id);
     client.log("Successfully Logged in as " + client.user.tag); // You can change the text if you want, but DO NOT REMOVE "client.user.tag"
     client.RegisterSlashCommands();
-  };
-  
-  name = !name;
-}, 10000);
+};
