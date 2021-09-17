29834a2b80e3dfa2f93e5fcfc95180b39c867eb9
Jônatas Souza Soares <56240097+jonatasJS@users.noreply.github.com>
2021-09-17 14:24:17 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,19 +1,28 @@
-let name=true;
-
-setInterval(() => {
-  name = !name;
-}, 10000);
-
 module.exports = async (client) => {
-    client.Ready = true;
+  let name = true;
+  
+	client.Ready = true;
+  client.user.setPresence({
+		status: "online", // You can show online, idle, and dnd
+		activity: {
+			name: name ? "Music (>play)" : "Help (>help)", // The message shown
+			type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING,
+		}
+	});
+  
+	setInterval(() => {
     client.user.setPresence({
-      status: "online",  // You can show online, idle, and dnd
+      status: "online", // You can show online, idle, and dnd
       activity: {
-          name: name ? "Music (>play)" : "Help (>help)",  // The message shown
-          type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING,
-        }
+        name: name ? "Music (>play)" : "Help (>help)", // The message shown
+        type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING,
+      }
     });
-    client.Manager.init(client.user.id);
-    client.log("Successfully Logged in as " + client.user.tag); // You can change the text if you want, but DO NOT REMOVE "client.user.tag"
-    client.RegisterSlashCommands();
+    
+		name = !name;
+	}, 10000);
+  
+	client.Manager.init(client.user.id);
+	client.log("Successfully Logged in as " + client.user.tag); // You can change the text if you want, but DO NOT REMOVE "client.user.tag"
+	client.RegisterSlashCommands();
 };
