bde36d42a38803fdfbbf27dc7acc970f9a6e97bf
Jônatas Souza Soares <56240097+jonatasJS@users.noreply.github.com>
2021-09-17 15:35:15 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,28 +1,40 @@
 module.exports = async (client) => {
-  let name = true;
-  
-	client.Ready = true;
-  client.user.setPresence({
-		status: "online", // You can show online, idle, and dnd
-		activity: {
-			name: name ? "Music (>play)" : "Help (>help)", // The message shown
-			type: "STREAMING", // PLAYING, WATCHING, LISTENING, STREAMING,
-		}
-	});
-  
-	setInterval(() => {
+    let name = "";
+    const myHeaders = new Headers();
+    const myInit = {
+        method: 'GET',
+        headers: myHeaders,
+        mode: 'cors',
+        cache: 'default'
+    };
+    const data = fetch(`/api/commands`, myInit).then(res => res.json());
+    const rondom = (min, max) => Math.floor(Math.random() * (min - max) + min);
+
+    client.Ready = true;
     client.user.setPresence({
-      status: "online", // You can show online, idle, and dnd
-      activity: {
-        name: name ? "Music (>play)" : "Help (>help)", // The message shown
-        type: "WATCHING", // PLAYING, WATCHING, LISTENING, STREAMING,
-      }
+        status: "online", // You can show online, idle, and dnd
+        activity: {
+            name: name ? "Music (>play)" : "Help (>help)", // The message shown
+            type: "STREAMING", // PLAYING, WATCHING, LISTENING, STREAMING,
+        }
     });
-    
-		name = !name;
-	}, 10000);
-  
-	client.Manager.init(client.user.id);
-	client.log("Successfully Logged in as " + client.user.tag); // You can change the text if you want, but DO NOT REMOVE "client.user.tag"
-	client.RegisterSlashCommands();
+
+    setInterval(async () => {
+        await data.commands.forEach((cmd) => {
+            const index = rondom(0, data.commands.length);
+            return name = `${await cmd[index].name} (${await cmd[index].aliases[1]})`;
+        });
+
+        client.user.setPresence({
+            status: "online", // You can show online, idle, and dnd
+            activity: {
+                name, // The message shown
+                type: "WATCHING", // PLAYING, WATCHING, LISTENING, STREAMING,
+            }
+        });
+    }, 10000);
+
+    client.Manager.init(client.user.id);
+    client.log("Successfully Logged in as " + client.user.tag); // You can change the text if you want, but DO NOT REMOVE "client.user.tag"
+    client.RegisterSlashCommands();
 };
