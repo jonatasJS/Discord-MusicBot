8b78ec0cfd6e7e7212b07bd4cef319984516a9d2
PatoDev_ <86863797+MrRicciGith@users.noreply.github.com>
2021-09-22 14:38:23 -0300

    Update loopqueue.js

--- commands/loopqueue.js
+++ commands/loopqueue.js
@@ -3,10 +3,10 @@ const { TrackUtils } = require("erela.js");
 
 module.exports = {
     name: "loopqueue",
-    description: "Loop the whole queue",
+    description: "Loop a fila inteira",
     usage: "",
     permissions: {
-      channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
+      channel: ["Ver Canal", "Enviar Mensagem", "Embed Links"],
       member: [],
     },
     aliases: ["lq", "repeatqueue", "rq"],
@@ -19,16 +19,16 @@ module.exports = {
       */
     run: async (client, message, args, { GuildDB }) => {
       let player = await client.Manager.get(message.guild.id);
-      if (!player) return client.sendTime(message.channel, "❌ | **Nothing is playing right now...**");
-      if (!message.member.voice.channel) return client.sendTime(message.channel, "❌ | **You must be in a voice channel to use this command!**");
-      if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return client.sendTime(message.channel, ":x: | **You must be in the same voice channel as me to use this command!**");
+      if (!player) return client.sendTime(message.channel, "❌ | **Nada está tocando agora...**");
+      if (!message.member.voice.channel) return client.sendTime(message.channel, "❌ | **Você deve estar em um canal de voz para usar este comando!**");
+      if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return client.sendTime(message.channel, ":x: | **Você deve estar no mesmo canal de voz que eu para usar este comando!**");
 
         if (player.queueRepeat) {
           player.setQueueRepeat(false)
-          client.sendTime(message.channel, `:repeat: Queue Loop \`disabled\``);
+          client.sendTime(message.channel, `:repeat: Queue Loop \`Desabilitado\``);
         } else {
           player.setQueueRepeat(true)
-          client.sendTime(message.channel, `:repeat: Queue Loop \`enabled\``);
+          client.sendTime(message.channel, `:repeat: Queue Loop \`Habilitado\``);
         }
     },
     SlashCommand: {
@@ -45,16 +45,16 @@ module.exports = {
           const member = guild.members.cache.get(interaction.member.user.id);
           const voiceChannel = member.voice.channel;
           let awaitchannel = client.channels.cache.get(interaction.channel_id); /// thanks Reyansh for this idea ;-;
-            if (!player) return client.sendTime(interaction, "❌ | **Nothing is playing right now...**"); 
-            if (!member.voice.channel) return client.sendTime(interaction, "❌ | **You must be in a voice channel to use this command.**");
-            if (guild.me.voice.channel && !guild.me.voice.channel.equals(voiceChannel)) return client.sendTime(interaction, ":x: | **You must be in the same voice channel as me to use this command!**");
+            if (!player) return client.sendTime(interaction, "❌ | **Nada está tocando agora...**"); 
+            if (!member.voice.channel) return client.sendTime(interaction, "❌ | **Você deve estar em um canal de voz para usar este comando**");
+            if (guild.me.voice.channel && !guild.me.voice.channel.equals(voiceChannel)) return client.sendTime(interaction, ":x: | **Você deve estar no mesmo canal de voz que eu para usar este comando!**");
 
             if(player.queueRepeat){
                   player.setQueueRepeat(false)
-                  client.sendTime(interaction, `:repeat: **Queue Loop** \`disabled\``);
+                  client.sendTime(interaction, `:repeat: **Queue Loop** \`Desabilitado\``);
               }else{
                   player.setQueueRepeat(true)
-                  client.sendTime(interaction, `:repeat: **Queue Loop** \`enabled\``);
+                  client.sendTime(interaction, `:repeat: **Queue Loop** \`Habilitado\``);
               }
           console.log(interaction.data)
         }
