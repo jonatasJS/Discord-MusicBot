c3c2857d27b70dc64f4098d56b317b64c7836c02
PatoDev_ <86863797+MrRicciGith@users.noreply.github.com>
2021-09-22 20:42:25 -0300

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -3,10 +3,10 @@ const { TrackUtils } = require("erela.js");
 
 module.exports = {
     name: "pause",
-    description: "Pauses the music",
+    description: "Pausa a música",
     usage: "",
     permissions: {
-        channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
+        channel: ["Ver Canal", "Enviar Mensagem", "Embed Links"],
         member: [],
     },
     aliases: [],
@@ -19,12 +19,12 @@ module.exports = {
      */
     run: async (client, message, args, { GuildDB }) => {
         let player = await client.Manager.get(message.guild.id);
-        if (!player) return client.sendTime(message.channel, "❌ | **Nothing is playing right now...**");
-        if (!message.member.voice.channel) return client.sendTime(message.channel, "❌ | **You must be in a voice channel to use this command!**");
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return client.sendTime(message.channel, ":x: | **You must be in the same voice channel as me to use this command!**");
+        if (!player) return client.sendTime(message.channel, "❌ | **Não está jogando nada agora...**");
+        if (!message.member.voice.channel) return client.sendTime(message.channel, "❌ | **Você deve estar em um canal de voz para usar este comando!**");
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return client.sendTime(message.channel, ":x: | **Você deve estar no mesmo canal de voz que eu para usar este comando!**");
         if (player.paused) return client.sendTime(message.channel, "❌ | **Music is already paused!**");
         player.pause(true);
-        let embed = new MessageEmbed().setAuthor(`Paused!`, client.botconfig.IconURL).setColor("RANDOM").setDescription(`Type \`${GuildDB.prefix}resume\` to continue playing!`);
+        let embed = new MessageEmbed().setAuthor(`Paused!`, client.botconfig.IconURL).setColor("RANDOM").setDescription(`Type \`${GuildDB.prefix}resume\` ontinuar escutando!`);
         await message.channel.send(embed);
         await message.react("✅");
     },
@@ -41,14 +41,14 @@ module.exports = {
             const guild = client.guilds.cache.get(interaction.guild_id);
             const member = guild.members.cache.get(interaction.member.user.id);
 
-            if (!member.voice.channel) return client.sendTime(interaction, "❌ | **You must be in a voice channel to use this command.**");
-            if (guild.me.voice.channel && !guild.me.voice.channel.equals(member.voice.channel)) return client.sendTime(interaction, ":x: | **You must be in the same voice channel as me to use this command!**");
+            if (!member.voice.channel) return client.sendTime(interaction, "❌ | **Você deve estar em um canal de voz para usar este comando.**");
+            if (guild.me.voice.channel && !guild.me.voice.channel.equals(member.voice.channel)) return client.sendTime(interaction, ":x: | **Você deve estar no mesmo canal de voz que eu para usar este comando!**");
 
             let player = await client.Manager.get(interaction.guild_id);
-            if (!player) return client.sendTime(interaction, "❌ | **Nothing is playing right now...**");
-            if (player.paused) return client.sendTime(interaction, "Music is already paused!");
+            if (!player) return client.sendTime(interaction, "❌ | **Não está jogando nada agora...**");
+            if (player.paused) return client.sendTime(interaction, "A música já está pausada!");
             player.pause(true);
-            client.sendTime(interaction, "**⏸ Paused!**");
+            client.sendTime(interaction, "**⏸ Pausado!**");
         },
     },
 };
