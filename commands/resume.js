3f2eac60bc13fb11b0ac2c1e0e57cff12e6bcfa4
PatoDev_ <86863797+MrRicciGith@users.noreply.github.com>
2021-10-03 12:33:23 -0300

    Update resume.js

--- commands/resume.js
+++ commands/resume.js
@@ -6,7 +6,7 @@ module.exports = {
     description: "Resumes the music",
     usage: "",
     permissions: {
-        channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
+        channel: ["Ver Canal", "Enviar Mensagem", "Embed Links"],
         member: [],
     },
     aliases: [],
@@ -19,11 +19,11 @@ module.exports = {
      */
     run: async (client, message, args, { GuildDB }) => {
         let player = await client.Manager.get(message.guild.id);
-        if (!player) return client.sendTime(message.channel, "❌ | **Nothing is playing right now...**");
-        if (!message.member.voice.channel) return client.sendTime(message.channel, "❌ | **You must be in a voice channel to use this command!**");
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return client.sendTime(message.channel, ":x: | **You must be in the same voice channel as me to use this command!**");
+        if (!player) return client.sendTime(message.channel, "❌ | **Não está tocando nada agora...**");
+        if (!message.member.voice.channel) return client.sendTime(message.channel, "❌ | **Você deve estar em um canal de voz para usar este comando!**");
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return client.sendTime(message.channel, ":x: | **Você deve estar no mesmo canal de voz que eu para usar este comando!**");
 
-        if (player.playing) return client.sendTime(message.channel, "❌ | **Music is already resumed!**");
+        if (player.playing) return client.sendTime(message.channel, "❌ | **A música já foi retomada!**");
         player.pause(false);
         await message.react("✅");
     },
@@ -40,14 +40,14 @@ module.exports = {
             const guild = client.guilds.cache.get(interaction.guild_id);
             const member = guild.members.cache.get(interaction.member.user.id);
 
-            if (!member.voice.channel) return client.sendTime(interaction, "❌ | **You must be in a voice channel to use this command.**");
-            if (guild.me.voice.channel && !guild.me.voice.channel.equals(member.voice.channel)) return client.sendTime(interaction, ":x: | **You must be in the same voice channel as me to use this command!**");
+            if (!member.voice.channel) return client.sendTime(interaction, "❌ | **Você deve estar em um canal de voz para usar este comando.**");
+            if (guild.me.voice.channel && !guild.me.voice.channel.equals(member.voice.channel)) return client.sendTime(interaction, ":x: | **Você deve estar no mesmo canal de voz que eu para usar este comando!**");
 
             let player = await client.Manager.get(interaction.guild_id);
-            if (!player) return client.sendTime(interaction, "❌ | **Nothing is playing right now...**");
-            if (player.playing) return client.sendTime(interaction, "❌ | **Music is already resumed!**");
+            if (!player) return client.sendTime(interaction, "❌ | **Não está tocando nada agora...**");
+            if (player.playing) return client.sendTime(interaction, "❌ | **A música já foi retomada!**");
             player.pause(false);
-            client.sendTime(interaction, "**⏯ Resumed!**");
+            client.sendTime(interaction, "**⏯ Retomado!**");
         },
     },
 };
