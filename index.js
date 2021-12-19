964a9e7562e9d80ee29936e3ad02bcde9a1ead0d
sozinho <soaresjonatas398@gmail.com>
2021-12-19 13:35:53 +0000

    REMOVED WEBSITE

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-require('dotenv').config();
-
 const DiscordMusicBot = require("./structures/DiscordMusicBot");
 const client = new DiscordMusicBot();
-/*
-const fs = require("fs");
-
-const { execSync, exec } = require('child_process');
-
-console.log("Execulted");
-
-const output = exec('yarn dev-next', { encoding: 'utf-8' });
-
-console.log('The output is:');
-console.clear();
-*/
 
 client.build();
-
-console.clear();
 
 module.exports = client; //;-;
